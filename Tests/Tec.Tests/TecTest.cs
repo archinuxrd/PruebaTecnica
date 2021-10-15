67fa47804a8f9e8e299c4516cece8bf5a9048dcc
Joel Mercedes <getro1888@gmail.com>
2021-10-14 21:14:09 -0400

    Fix

--- Tests/Tec.Tests/TecTest.cs
+++ Tests/Tec.Tests/TecTest.cs
@@ -14,40 +14,7 @@ namespace Tec.Tests
         [Fact]
         public void Test1()
         {
-            // //Setup DbContext and DbSet mock  
-            // var dbContext = new Mock<ApplicationDbContext>(); 
-            // var dbSetMock = new Mock<DbSet<Product>>();
-            // dbSetMock.Setup(s => s.FindAsync(It.IsAny<int>()));  
-            // dbContext.Setup(s => s.Set<Product>()).Returns(dbSetMock.Object);  
             //
-            // //Execute method of SUT (ProductsRepository)  
-            // var productRepository = new ProductRepository(dbContext.Object);  
-            // var product = productRepository.FirstAsync(x=>x.Id == 1).Result;  
-            //
-            // //Assert  
-            // Assert.NotNull(product);  
-            // Assert.IsAssignableFrom<Product>(product);
-        }
-        
-        [Fact]
-        public void Can_add_item()
-        {
-            using (var context = new ItemsContext(ContextOptions))
-            {
-                var controller = new ItemsController(context);
-
-                var item = controller.PostItem("ItemFour").Value;
-
-                Assert.Equal("ItemFour", item.Name);
-            }
-
-            using (var context = new ItemsContext(ContextOptions))
-            {
-                var item = context.Set<Item>().Single(e => e.Name == "ItemFour");
-
-                Assert.Equal("ItemFour", item.Name);
-                Assert.Equal(0, item.Tags.Count);
-            }
         }
     }
 }
